a9c27069f1249fd4471e742f539b7e29a1e9f7c6
Eve Redero <eve.redero@gmail.com>
2024-07-04 12:38:48 +0200

    wip: batch reading

--- adc_dt/src/main.c
+++ adc_dt/src/main.c
@@ -37,8 +37,9 @@ adc_sequence_callback adc_callback(const struct device *dev,
 		uint16_t sampling_index)
 {
 	if (sampling_index == 31) {
+		/* Total number of samples reached (for all channels) */
 		LOG_INF("Sampling %d", sampling_index);
-		LOG_HEXDUMP_INF(sequence->buffer, 32, "Buffer:");
+		LOG_HEXDUMP_DBG(sequence->buffer, 32 * 6, "Buffer:");
 	}
 	return(ADC_ACTION_CONTINUE);
 
@@ -48,9 +49,9 @@ int main(void)
 {
 	int err;
 	uint32_t count = 0;
-	uint16_t buf[32];
+	uint16_t buf[32 * 6];
 	const struct adc_sequence_options adc_options = {
-		.interval_us = 1000,
+		.interval_us = 100000,
 		.callback = &adc_callback,
 		/* How many to read -1 */
 		.extra_samplings = 31,
@@ -60,39 +61,48 @@ int main(void)
 		/* buffer size in bytes, not number of samples */
 		.buffer_size = sizeof(buf),
 		.options = &adc_options,
-		.channels = 0b11111001, /* 0xf9, adc channels bitmask */
+		.channels = 0xf9, /* 0b111110011, adc channels bitmask */
 	};
 
 	/* Configure channels individually prior to sampling. */
 	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
 		if (!adc_is_ready_dt(&adc_channels[i])) {
-			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+			LOG_ERR("ADC controller device %s not ready\n", adc_channels[i].dev->name);
 			return 0;
 		}
 
 		err = adc_channel_setup_dt(&adc_channels[i]);
 		if (err < 0) {
-			printk("Could not setup channel #%d (%d)\n", i, err);
+			LOG_ERR("Could not setup channel #%d (%d)\n", i, err);
 			return 0;
 		}
 	}
-	/* Read each channel once, with 32 samples in each */
-		printk("ADC reading[%u]:\n", count++);
+		/* Initializes all channels */
 		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-			int32_t val_mv;
+			//int32_t val_mv;
 
 			printk("- %s, channel %d: \n",
 			       adc_channels[i].dev->name,
 			       adc_channels[i].channel_id);
-
+			//LOG_INF("1- 0x%x", sequence.channels);
 			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+			//LOG_INF("2- 0x%x", sequence.channels);
+		}
+		/* Fix: reset sequence.channels to the value we wanted */
 
+		sequence.channels = 0xf9;
+		/* Perform 10 batch readings of 32 samples on all channels*/
+		for (size_t i = 0U; i < 10; i++) {
 			err = adc_read_dt(&adc_channels[i], &sequence);
-			k_sleep(K_MSEC(1000));
 			if (err < 0) {
 				printk("Could not read (%d)\n", err);
 				continue;
 			}
-}
+			for (i = 0; i < 32; i++) {
+				/* Print 32 values from 2nd ADC channel */
+				printk("%d ", ((int16_t*)sequence.buffer)[1 + i * 6]);
+			}
+			k_sleep(K_MSEC(1000));
+		}
 	return 0;
 }
