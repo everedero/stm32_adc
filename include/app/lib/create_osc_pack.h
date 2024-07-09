a7165f9abd25fecca9bf8f5f1ae7201d7514fff7
Eve Redero <eve.redero@gmail.com>
2024-07-09 18:53:34 +0200

    cleanup: remove headers and copyrights

--- include/app/lib/create_osc_pack.h
+++ include/app/lib/create_osc_pack.h
@@ -1,4 +1,8 @@
-
+/*
+ * Copyright (c) 2024 Eve Redero
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 int create_msg(char *tx_buffer, int max_len);
 void read_msg(char* buffer, int len);
--- lib/tinyosc/create_osc_pack.c
+++ lib/tinyosc/create_osc_pack.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Eve Redero
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "tinyosc.h"
 #include <zephyr/kernel.h>
 
